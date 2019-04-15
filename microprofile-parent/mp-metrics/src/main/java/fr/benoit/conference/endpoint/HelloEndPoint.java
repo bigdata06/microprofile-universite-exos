1dd4a8f258b199b6202539c32877f5d1b6e02555
lbenoit <lilian.benoit@lbenoit.fr>
2019-04-15 20:00:52 +0200

    Mise en place des exemples MP Check

--- microprofile-parent/mp-metrics/src/main/java/fr/benoit/conference/endpoint/HelloEndPoint.java
+++ microprofile-parent/mp-metrics/src/main/java/fr/benoit/conference/endpoint/HelloEndPoint.java
@@ -1,5 +1,7 @@
 package fr.benoit.conference.endpoint;
 
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -10,12 +12,23 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.metrics.Counter;
+import org.eclipse.microprofile.metrics.annotation.Counted;
+import org.eclipse.microprofile.metrics.annotation.Metric;
+import org.eclipse.microprofile.metrics.annotation.Timed;
+
 @Path("/hello")
+@RequestScoped
 public class HelloEndPoint {
 
+	@Inject
+	@Metric(name="sayHello")
+	Counter cpt;
+	
 	@GET
 	@Path("/{prenom}")
 	@Produces(MediaType.TEXT_PLAIN)
+	@Counted(name="sayHello", absolute=true, reusable=true, monotonic=true)
 	public String sayHello(@PathParam("prenom") String prenom) {
 		return "Hello " + prenom;
 	}
@@ -24,6 +37,7 @@ public class HelloEndPoint {
 	@Path("/json")
 	@Produces(MediaType.APPLICATION_JSON)
 	public JsonObject sayHelloJson() {
+		cpt.inc();
 		JsonObjectBuilder builder = Json.createObjectBuilder();
 		builder.add("message", "Hello World");
 		
@@ -35,5 +49,19 @@ public class HelloEndPoint {
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response sayHelloResponse() {
 		return Response.ok("Hello World").build();
+	}
+	
+	@Timed
+	@GET
+	@Path("/patience")
+	@Produces(MediaType.TEXT_PLAIN)
+	public String sayHelloAfter() {
+		long rand = (long)(Math.random() * 10);
+		try {
+			Thread.sleep(rand * 1000);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return String.format("Bonjour avec un peu de patience (%ds)", rand);
 	}
 }
