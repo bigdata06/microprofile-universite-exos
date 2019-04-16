dd252bd8a53b72fb1e2654b7c68b3df90f82128b
lbenoit <lilian.benoit@lbenoit.fr>
2019-04-16 10:59:12 +0200

    Exemple pour MP-FaultTolerance

--- microprofile-parent/mp-fault-tolerance/src/main/java/fr/benoit/conference/endpoint/HelloEndPoint.java
+++ microprofile-parent/mp-fault-tolerance/src/main/java/fr/benoit/conference/endpoint/HelloEndPoint.java
@@ -1,5 +1,6 @@
 package fr.benoit.conference.endpoint;
 
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -10,22 +11,32 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import fr.benoit.conference.service.BadService;
+
 @Path("/hello")
 public class HelloEndPoint {
 
+	@Inject
+	BadService service;
+	
 	@GET
 	@Path("/{prenom}")
 	@Produces(MediaType.TEXT_PLAIN)
 	public String sayHello(@PathParam("prenom") String prenom) {
-		return "Hello " + prenom;
+		long rand = service.methodeSuperLongue();
+		return String.format("Hello %s (%d s)",prenom,rand);
 	}
 	
 	@GET
 	@Path("/json")
 	@Produces(MediaType.APPLICATION_JSON)
 	public JsonObject sayHelloJson() {
+		BadService.nbFois=0;
+		int nb= service.methodePasFiable();
 		JsonObjectBuilder builder = Json.createObjectBuilder();
 		builder.add("message", "Hello World");
+		builder.add("nb", nb);
+		builder.add("nbFois", BadService.nbFois);
 		
 		return builder.build();
 	}
--- microprofile-parent/mp-fault-tolerance/src/main/java/fr/benoit/conference/service/BadService.java
+++ microprofile-parent/mp-fault-tolerance/src/main/java/fr/benoit/conference/service/BadService.java
@@ -1,14 +1,19 @@
 package fr.benoit.conference.service;
 
+import java.time.temporal.ChronoUnit;
 import java.util.Random;
 
 import javax.enterprise.context.Dependent;
+
+import org.eclipse.microprofile.faulttolerance.Retry;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 
 @Dependent
 public class BadService {
 
 	static public int nbFois =0;
 	
+	@Timeout(value=3, unit=ChronoUnit.SECONDS)
 	public int methodeSuperLongue() {
 		int wait = new Random().nextInt(5)+1;
 		try {
@@ -19,6 +24,7 @@ public class BadService {
 		return wait;
 	}
 
+	@Retry(maxRetries=10)
 	public int methodePasFiable() {
 		int nb = new Random().nextInt(1000)/2;
 		nbFois++;
