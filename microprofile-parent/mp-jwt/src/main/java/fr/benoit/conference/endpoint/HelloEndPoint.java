98992e35828a88485a877a5186135dab9ba24d8a
lbenoit <lilian.benoit@lbenoit.fr>
2019-04-16 19:16:07 +0200

    Mise en place de la solution MP-JWT

--- microprofile-parent/mp-jwt/src/main/java/fr/benoit/conference/endpoint/HelloEndPoint.java
+++ microprofile-parent/mp-jwt/src/main/java/fr/benoit/conference/endpoint/HelloEndPoint.java
@@ -1,5 +1,8 @@
 package fr.benoit.conference.endpoint;
 
+import javax.annotation.security.RolesAllowed;
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
 import javax.json.Json;
 import javax.json.JsonObject;
 import javax.json.JsonObjectBuilder;
@@ -10,9 +13,15 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.eclipse.microprofile.jwt.JsonWebToken;
+
 @Path("/hello")
+@RequestScoped
 public class HelloEndPoint {
 
+	@Inject
+	JsonWebToken token;
+	
 	@GET
 	@Path("/{prenom}")
 	@Produces(MediaType.TEXT_PLAIN)
@@ -27,12 +36,16 @@ public class HelloEndPoint {
 		JsonObjectBuilder builder = Json.createObjectBuilder();
 		builder.add("message", "Hello World");
 		
+		if (token != null) {
+			builder.add("username", token.getName());
+		}
 		return builder.build();
 	}
 	
 	@GET
 	@Path("/response")
 	@Produces(MediaType.APPLICATION_JSON)
+	@RolesAllowed("admin")
 	public Response sayHelloResponse() {
 		return Response.ok("Hello World").build();
 	}
